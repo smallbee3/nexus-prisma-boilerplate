3830a898dac791e81b4a5e958474476248d89041
smallbee <smallbee3@gmail.com>
2020-05-06 00:04:13 +0900

    Add 6 crud mutation types

--- api/graphql.ts
+++ api/graphql.ts
@@ -38,3 +38,15 @@ schema.queryType({
     })
   }
 })
+
+schema.mutationType({
+  definition(t) {
+    t.crud.createOneWorld()
+    t.crud.updateOneWorld()
+    t.crud.upsertOneWorld()
+    t.crud.deleteOneWorld()
+
+    t.crud.updateManyWorld()
+    t.crud.deleteManyWorld()
+  }
+})
